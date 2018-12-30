006a94b805a736b5e6addba6b6bd55c73db994f6
lanasgoddard <lanasgoddard@gmail.com>
2018-12-30 15:27:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,18 +17,6 @@ function nowServing(katzDeli) {
 
 var line = []
 
-function currentLine(katzDeli){
-  var n = 0
-  while(n<katzDeli.length)
-  n++
-  if(katzDeli.length === 0){
-    return 'The line is currently empty';
-  }
-    else
-    return (`The line is currently: ${n} ${katzDeli} `)
-    }
-  }
-
   function currentLine(katzDeli) {
   for (let i = 0; i < katzDeli.length; i++) {
     line.push(` `+[i+1]+`. `  + katzDeli[i])
