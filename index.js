a4cd08b68f83a22d7081403c257100a2bbd55236
lanasgoddard <lanasgoddard@gmail.com>
2018-12-30 14:40:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,10 @@ function nowServing(katzDeli) {
   else
   return (`Currently serving ${katzDeli.shift()}.`);
 }
+
+function currentLine(katzDeli){
+  return `The line is currently: (x)(x)`
+  if(katzDeli.length === 0){
+    return 'The line is currently empty'
+  }
+}
