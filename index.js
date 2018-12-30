2b723060d1f20788c57f513134e5e0a2f3c8cfbc
lanasgoddard <lanasgoddard@gmail.com>
2018-12-30 14:00:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
+
 function nowServing(katzDeli) {
   let i = 0;
   while (i < katzDeli.length) {
     i++;
   }
   if (katzDeli.length === 0) {
-    return ("The line is currently empty!");
+    return "The line is empty";
   }
   else
   return (`Currently serving ${katzDeli.shift()}.`);
+}
