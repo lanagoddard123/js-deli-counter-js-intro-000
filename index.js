550e18ef2f9895fc11bb7eb88d10293e3ffe9a84
lanasgoddard <lanasgoddard@gmail.com>
2018-12-30 14:01:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeli) {
     i++;
   }
   if (katzDeli.length === 0) {
-    return "The line is empty";
+    return "There is nobody waiting to be served";
   }
   else
   return (`Currently serving ${katzDeli.shift()}.`);
